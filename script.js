db8deccb31b2315ca70dc1d92e33bd9497fce7d8
Kirill <nightmareasgard@gmail.com>
2025-09-14 10:04:36 +0500

    added "function" + "while" start to a question

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict"
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start () {
+   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+   while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+       numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+   }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -11,34 +21,18 @@ const personalMovieDB = {
 };
 
 
-// for (let i = 0; i < 2; i++) {
-//  const a = prompt('Один из последних просмотренных фильмов?', ''),
-//        b = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', '');
 
-//        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//        } else {
-//         console.log('error');
-//         i--;
-//        }
-// }
-
-let i = 0;
-do {
-    const a = prompt('Один из последних просмотренных фильмов?', '');
-    const b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+       if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('done');
-        i++;
-    } else {
+       } else {
         console.log('error');
-    }
-} while (i < 2);
-
-
+        i--;
+       }
+}
 
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
